6023dd38978a175c7197c56a05cb5c7303cf0e48
katsuyukishimbo <katsuyuki0611@gmail.com>
2019-04-17 10:07:37 +0900

    add header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,12 +1,27 @@
 import Link from 'next/link';
+import {
+  withStyles,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton
+} from  '@material-ui/core';
 
 export default ({ pathname }: { pathname?: any }) => (
   <header>
-    <Link href='/'>
-      <a className={pathname === '/' ? 'is-active' : ''}>Home</a>
-    </Link>
-    <Link href='/about'>
-      <a className={pathname === '/about' ? 'is-active' : ''}>About</a>
-    </Link>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton color="inherit" aria-label="Menu">
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            News
+          </Typography>
+          <Link href='/about'>
+            <a className={pathname === '/about' ? 'is-active' : ''}>About</a>
+          </Link>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
   </header>
 );
